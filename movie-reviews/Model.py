629c67f49a77fe02afdfdcba8053d19b7576f9ba
AdrianPlesner <Adrian.plesner@gmail.com>
2021-11-03 17:13:46 +0100

    Initial movie-reviews

---
+++ movie-reviews/Model.py
@@ -0,0 +1,43 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+
+class Model(Sequential):
+
+    def new(self, input_dim):
+        self.input_dim = input_dim
+        self.add(layers.Dense(10, input_dim=input_dim, activation='relu'))
+        self.add(layers.Dense(64, activation='relu'))
+        self.add(layers.Dense(128, activation='relu'))
+        self.add(layers.Dense(64, activation='relu'))
+        self.add(layers.Dense(1, activation='relu'))
+        self.compile(loss='mean_absolute_error', optimizer='adam', metrics=['accuracy'])
+
+
+    def forward(self, sample):
+        sample = self._preprocess_data(sample)
+        return self.predict(sample)
+
+
+    def _preprocess_data(self, data):
+        data = self.vectorizer.transform([data])
+        return data
+
+    def save_model(self, save_path):
+        with open(save_path + "/input_dim", 'wb') as fp:
+            pickle.dump(self.input_dim, fp)
+        with open(save_path + "/vocabulary", 'wb') as fp:
+            pickle.dump(self.vectorizer.vocabulary_, fp)
+        self.save_weights(save_path + "/weights.json")
+
+
+    def load_model(self, model_path):
+        with open(model_path + "/input_dim", 'rb') as fp:
+            input_dim = pickle.load(fp)
+        with open(model_path + "/vocabulary", 'rb') as fp:
+            v = pickle.load(fp)
+            self.vectorizer = CountVectorizer(vocabulary=v)
+        self.new(input_dim)
+        self.load_weights(model_path + "/weights.json")
+
---
+++ movie-reviews/Trainer.py
@@ -0,0 +1,43 @@
+from Model import Model
+import pandas
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+
+
+class Trainer:
+
+    def __init__(self, model_path=None):
+        if model_path is not None:
+            self.model.load_model(model_path)
+
+    def _load_train_data(self, dataset_path):
+        train_dataset = pandas.read_csv(dataset_path)
+        train_dataset = train_dataset.drop(columns=['Unnamed: 0'])
+        return train_dataset
+
+    def _preprocess_train_data(self, train_data):
+        reviews = train_data['review_content'].values
+        scores = train_data['score'].values
+        reviews_train, reviews_test, scores_train, scores_test = train_test_split(
+            reviews, scores, test_size=0.33, random_state=1000)
+        vectorizer = CountVectorizer()
+        vectorizer.fit(reviews_train)
+        self.vectorizer = vectorizer
+        X_train = vectorizer.transform(reviews_train)
+        X_test = vectorizer.transform(reviews_test)
+        self.test_data = X_test, scores_test
+        return X_train, scores_train
+
+    def train(self, data_path, save_path):
+        data = self._load_train_data(data_path)
+        x, y = self._preprocess_train_data(data)
+        self.model = Model()
+        self.model.new(x.shape[1])
+        self.model.vectorizer = self.vectorizer
+        print(x.shape[0])
+        history = self.model.fit(x, y, epochs=32, verbose=True, validation_data=self.test_data, batch_size=1000)
+        self.model.save_model(save_path)
+        train_loss, train_accuracy = self.model.evaluate(x, y, verbose=True)
+        test_loss, test_accuracy = self.model.evaluate(self.test_data[0], self.test_data[1], verbose=True)
+        return {'train_loss': train_loss, 'train_accuracy': train_accuracy, 
+            'test_loss': test_loss, 'test_accuracy': test_accuracy}
